57e1346743fb8a849f4d76670fe5709004073e15
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2023-05-06 17:37:09 -0300

    ajuste do schema

--- src/schemas/autores.js
+++ src/schemas/autores.js
@@ -2,16 +2,8 @@ const mongoose = require(`mongoose`);
 
 const AutoresSchema = mongoose.Schema({
   id: { type: Number, required: true, unique: true },
-  uri: { type: String },
-  siglaTipo: { type: String, required: true },
-  codTipo: { type: Number, required: true },
-  numero: { type: Number },
-  ano: { type: Number },
-  ementa: { type: String },
-  deputado: [{
-    id: { type: 'string', required: true, unique: true },
-    nome: { type: 'string', required: true },
-  }],
+  nome: { type: String, required: true },
+  tipo: { type: String, required: true },
 });
 
 AutoresSchema.pre('save', async function (next) {
