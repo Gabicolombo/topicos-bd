f45c26e3b98bb33edd9ff45a2248f7f1caa13898
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2023-05-15 20:34:44 -0300

    deputado por id

--- src/routes/deputados.js
+++ src/routes/deputados.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/deputados');
 
 router.get('/deputados', controller.getDeputados);
 router.get('/getListaDeputados/:page/:quantity', controller.getListaDeputados);
+router.get('/deputados/:id', controller.getDeputado);
 
 module.exports = router;
