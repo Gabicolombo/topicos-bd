5a03d5d2d2835d68031cc5886ad51f636aabea55
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2023-05-20 16:39:50 -0300

    ultima pagina salva

--- src/controllers/propostas.js
+++ src/controllers/propostas.js
@@ -1,46 +1,59 @@
 const Propostas = require('../schemas/propostas');
+const Deputados = require('../schemas/deputados');
 const axios = require('axios');
 
-const getPropostas = async(req, res, next) => {
-    try{
-        let pagina = 1812;
+const getPropostas = async (req, res, next) => {
+    try {
+        let pagina = 5029;
+
         let dados = ['']
-        while(dados.length > 0) {
+        while (dados.length > 0) {
 
             const result = await axios.get(`${process.env.API_URL}/proposicoes?ordem=ASC&ordenarPor=id&ano=2019&ano=2020&ano=2021&ano=2022&pagina=${pagina}`);
-         
-            if(result.data.dados.length == 0) return;
 
-            for(doc of result.data.dados){
-                
+            if (result.data.dados.length == 0) return;
+
+            for (doc of result.data.dados) {
+
                 let author = await axios.get(`${process.env.API_URL}/proposicoes/${doc.id}/autores`);
                 let id = doc.id;
-                
-                const deputados = author.data.dados.map((deputado) => {
-                    let id = deputado.uri.split('/').pop();
-                    let nome = deputado.nome;
-                    if(id == '' || id == undefined) id = '-';
-                    if(nome == '' || id == undefined) nome = '-';
-                    return { id, nome };
-                });
-                
-                delete doc['id'];
-                let document = {
-                    ...doc,
-                    idProposicao: id,
-                    deputados: deputados
+
+                const deputados = await Promise.all(author.data.dados.map(async (deputado) => {
+                    const id = deputado.uri.split('/').pop();
+                    const result = await Deputados.find({ id: id });
+                    if (result.length > 0) {
+                        let nome = deputado.nome;
+                        let tipo = deputado.tipo;
+                        if (id == '' || id == undefined) id = '-';
+                        if (nome == '' || nome == undefined) nome = '-';
+                        if (tipo == '' || tipo == undefined) nome = '-';
+                        return { id, nome, tipo };
+                    }
+                }));
+                if (deputados.length > 0 && deputados[0] != undefined) {
+
+
+                    delete doc['id'];
+                    let document = {
+                        ...doc,
+                        idProposicao: id,
+                        deputados: deputados
+                    }
+
+                    await Propostas.create(document);
+
+
                 }
 
-                await Propostas.create(document);
             }
             console.log(pagina);
             pagina++;
-    
+
         }
 
         return res.status(200).json(dados);
     }
-    catch(err){
+    catch (err) {
         console.error(err);
         next();
     }
