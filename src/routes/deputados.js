7f0c15d95b40d1aa9c05059b2cf1e1d3ee7adfaf
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2023-05-31 16:17:08 -0300

    mudança do tema

--- src/routes/deputados.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/deputados');
-
-router.get('/deputados', controller.getDeputados);
-router.get('/getListaDeputados/:page/:quantity', controller.getListaDeputados);
-router.get('/deputados/:id', controller.getDeputado);
-
-module.exports = router;
