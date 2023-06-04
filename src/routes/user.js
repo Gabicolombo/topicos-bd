f86d5a14f2caf719d061d17e1905d0cd0cb97d67
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2023-06-04 15:20:16 -0300

    criando rotas para testar para salvar as mensagens, antes de utilizar o socket

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,10 +2,17 @@ const express = require('express');
 const controller = require('../controllers/user');
 const auth = require('../middleware/auth');
 
+const multer = require('multer');
+
+const upload = multer({ dest: '../uploads/' });
+
 const routes = express.Router();
 
 routes.post('/cadastro', controller.register);
 routes.post('/login', controller.login);
 routes.delete('/sair', auth, controller.removeToken);
 
+//upload.single('imagem')
+routes.post('/messages', auth, upload.single('imagem'), controller.saveMessage);
+routes.get('/messages', controller.getImage);
 module.exports = routes;
