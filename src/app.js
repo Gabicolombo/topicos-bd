2df4c56b0d5a79789259fc63837a9141c577b286
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2023-06-11 16:11:43 -0300

    começando a configurar o socket

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ const bodyparser = require('body-parser');
 const db = require('./database/mongodb');
 const path = require('path'); 
 const dotenv = require('dotenv');
+const http = require('http');
+const { Socket } = require('./socket');
+// para ir atualizando a pagina
+const cron = require('node-cron');
+
 
 dotenv.config({ path: path.resolve('./env/config.env')});
 
@@ -13,16 +18,34 @@ const port = process.env.PORT;
 // configurando as rotas
 const routesUsuarios = require('../src/routes/user');
 
-
 // conectando
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended:false}));
+
+// // preparando a conexão cliente e servidor
+// const socket = new Socket();
+// const serverHttp = http.createServer(app);
+// socket.init(serverHttp);
+
 
 app.use(routesUsuarios);
 
 db();
+
+cron.schedule('* * * * *', () => {
+  const rooms = Socket.rooms;
+  Object.keys(rooms).forEach(async (key) => {
+    if ('startDate' in rooms[key] && 'hasStarted' in rooms[key]) {
+      const { startDate, hasStarted } = rooms[key];
+      if (new Date() > new Date(startDate) && hasStarted === false) {
+        await Socket.startRoomTime(key);
+      }
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
