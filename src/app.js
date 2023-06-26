b53e54cf7be46980dade320c0a1590baf69d7e8c
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2023-06-26 11:14:17 -0300

    mexendo nas travas do cors para ter a conexão com o frontend

--- src/app.js
+++ src/app.js
@@ -6,35 +6,30 @@ const path = require('path');
 const dotenv = require('dotenv');
 const http = require('http');
 const app = express();
-const server = http.createServer(app);
-// para ir atualizando a pagina
 const cron = require('node-cron');
+const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+});
 const userBusiness = require('./repositories/user');
 const fs = require('fs');
 
+dotenv.config({ path: path.resolve('./env/config.env') });
 
-dotenv.config({ path: path.resolve('./env/config.env')});
-
-// configurando a porta
 const port = process.env.PORT;
 
-// configurando as rotas
 const routesUsuarios = require('../src/routes/user');
 
-// conectando
+app.use(cors()); // Usar o middleware cors
 
-app.use(cors());
 app.use(express.json());
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended:false}));
-
-// // preparando a conexão cliente e servidor
-// const socket = new Socket();
-// const serverHttp = http.createServer(app);
-// socket.init(serverHttp);
-
+app.use(bodyparser.urlencoded({ extended:false }));
 
 app.use(routesUsuarios);
 
@@ -56,13 +51,13 @@ server.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 //conexao com o socket
 io.on('connection', (socket) => {
-
+  console.log('connected');
   //recebimento da mensagem
   socket.on('chat message', (data) => {
     console.log(data);
